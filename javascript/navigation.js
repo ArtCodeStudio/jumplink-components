316bb08943a5ab31423190e526f8dd32cdb965c0
Pascal Garber <pascal@jumplink.eu>
2016-11-14 08:39:01 -0500

    added navigation.js and styleicons.js

---
+++ javascript/navigation.js
@@ -0,0 +1,13 @@
+/**
+ * Performs a smooth page scroll to an anchor on the same page.y
+ * @see https://css-tricks.com/snippets/jquery/smooth-scrolling/
+ */
+ 
+var initSmoothPageScroll = function () { 
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return true;
+    });
+}
